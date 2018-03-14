71bcd06f47cf18b904e2b00b584b6331c9e42a19
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2018-03-14 09:58:40 -0700

    Experimental support for Transfer-Encoding: chunked

--- src/org/sample/WebServicesSOAPActionAndEnvelopeHashIdGenerator.java
+++ src/org/sample/WebServicesSOAPActionAndEnvelopeHashIdGenerator.java
@@ -7,14 +7,18 @@ package org.sample;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
 
 import com.ibm.websphere.servlet.cache.CacheConfig;
 import com.ibm.websphere.servlet.cache.IdGenerator;
@@ -28,6 +32,8 @@ public class WebServicesSOAPActionAndEnvelopeHashIdGenerator implements IdGenera
 
 	private static final String ACTION = "action=";
 	private static final MessageDigest hasher;
+
+	private static final boolean EXPERIMENTAL_SUPPORT_TRANSFER_ENCODING_CHUNKED = true;
 
 	static {
 		try {
@@ -47,6 +53,7 @@ public class WebServicesSOAPActionAndEnvelopeHashIdGenerator implements IdGenera
 	 * id does not exist in the cache (CACHE MISS) or simply returning the cached
 	 * response for that id from the cache.
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public String getId(ServletCacheRequest request) {
 
@@ -62,6 +69,29 @@ public class WebServicesSOAPActionAndEnvelopeHashIdGenerator implements IdGenera
 
 			if (soapAction != null) {
 				try {
+					if (EXPERIMENTAL_SUPPORT_TRANSFER_ENCODING_CHUNKED) {
+						try {
+							ServletRequest req = ((ServletRequestWrapper) request).getRequest();
+							InputStream in = req.getInputStream();
+							if (req.getContentLength() < 0) {
+								@SuppressWarnings("rawtypes")
+								HashMap inStreamInfo = (HashMap) req.getClass().getMethod("getInputStreamData")
+										.invoke(req);
+								int len = in.available();
+								if (len < 0) {
+									Class<?> dinClass = Class.forName("DetachableInputStream");
+									Object din = dinClass.getConstructor(InputStream.class).newInstance(in);
+									din.getClass().getMethod("detach").invoke(din);
+									len = (int) din.getClass().getMethod("available").invoke(din);
+								}
+								inStreamInfo.put("ContentDataLength", new Long(len));
+								req.getClass().getMethod("setInputStreamData", HashMap.class).invoke(req, inStreamInfo);
+							}
+						} catch (Throwable t) {
+							throw new RuntimeException(t);
+						}
+					}
+
 					request.setGeneratingId(true); // ** DO NOT REMOVE... THIS IS NEEDED FOR PROPER PARSING OF RESPONSE
 
 					byte[] buffer = new byte[1024];
